7e3cd9771c523b691cc118d531742afc577aa926
CrystalOfLife <nicolai7@godmail.dk>
2018-04-18 13:48:51 +0200

    timer

---
+++ Assets/Assets/Scripts/Gui.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gui : MonoBehaviour {
+
+    private float timerValue;
+    public Text timer;
+
+    private void Start()
+    {
+        timer = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        timerValue += Time.deltaTime;
+        timer.text = "survive: " + timerValue;
+    }
+}
